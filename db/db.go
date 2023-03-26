620e2f30839705b64eb2657756463b04b96d8665
JackNLewis <jacklewis2809@gmail.com>
2023-03-26 18:43:14 +0100

    updated login handler

--- db/db.go
+++ db/db.go
@@ -1,30 +1,28 @@
 package db
 
-import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
+// import (
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
 
-type User struct {
-	ID           uint
-	Username     string
-	PasswordHash string
-	PasswordSalt string
-	Email        string
-}
+// type User struct {
+// 	ID           uint
+// 	Username     string
+// 	PasswordHash string
+// 	Email        string
+// }
 
-var (
-	SqlDB *gorm.DB
-)
+// var (
+// 	SqlDB *gorm.DB
+// )
 
-func InitDB() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	SqlDB, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// func InitDB() {
+// 	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+// 	SqlDB, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// }
 
-}
-
-func GetUser(username string) *User {
-	var user *User
-	SqlDB.Where("name = ?", username).First(&user)
-	return user
-}
+// func GetUser(username string) *User {
+// 	var user *User
+// 	SqlDB.Where("name = ?", username).First(&user)
+// 	return user
+// }
--- main.go
+++ main.go
@@ -4,23 +4,23 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/JackNLewis/auth-backend/db"
-	"github.com/JackNLewis/auth-backend/session"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 
+	// log.Print(hashAndSalt([]byte("password")))
 	//pass ports as the flag
 
 	//Initialise db connection
 
 	//Initialise session store
-	session.InitSession()
-	defer session.Store.Close()
+	// session.InitSession()
+	// defer session.Store.Close()
 
 	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/signup", signupHandler)
-	http.HandleFunc("/home", defaultHandler)
+	// http.HandleFunc("/signup", signupHandler)
+	// http.HandleFunc("/home", defaultHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -34,51 +34,82 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	r.ParseForm()
 
-	// Get a session. Get() always returns a session, even if empty.
-	session, err := session.Store.Get(r, "user")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	//retrieve username and password from database
-	// if r.Form.Get("username") != "username" || r.Form.Get("password") != "password" {
-	// 	log.Print("user authentification failed")
-	// 	http.Error(w, "user login failed", http.StatusInternalServerError)
+	// // Get a session. Get() always returns a session, even if empty.
+	// session, err := session.Store.Get(r, "user")
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
 	// 	return
 	// }
 
-	user := db.GetUser(r.Form.Get("username"))
-	if user == nil { //couldn't find user with that username
-		log.Printf("could not find user %v", r.Form.Get("username"))
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	// user := db.GetUser(r.Form.Get("username"))
+	// if user == nil { //couldn't find user with that username
+	// 	log.Printf("could not find user %v", r.Form.Get("username"))
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
-	// Save it before we write to the response/return from the handler.
-	err = session.Save(r, w)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// ok := comparePasswords(user.PasswordHash, []byte(r.Form.Get("password")))
+	// if !ok {
+	// 	log.Printf("invalid password for %v", r.Form.Get("username"))
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// session.Values["UserID"] = user.ID
+	// // Save it before we write to the response/return from the handler.
+	// err = session.Save(r, w)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
 }
 
 // signupHandler checks the credentials are correct and inserts the new user into the database
-func signupHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("signup handler"))
+// func signupHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("signup handler"))
+// }
+
+// // defaultHandler returns the main json data which will be used to render the main page
+// func defaultHandler(w http.ResponseWriter, r *http.Request) {
+// 	session, err := session.Store.Get(r, "user")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	if session.Values["UserID"] != 1234 {
+// 		http.Redirect(w, r, "http://localhost:8080/login", http.StatusTemporaryRedirect)
+// 		return
+// 	}
+
+// 	w.Write([]byte("home handler"))
+// }
+
+func hashAndSalt(pwd []byte) string {
+
+	// Use GenerateFromPassword to hash & salt pwd.
+	// MinCost is just an integer constant provided by the bcrypt
+	// package along with DefaultCost & MaxCost.
+	// The cost can be any value you want provided it isn't lower
+	// than the MinCost (4)
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	// GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it
+	return string(hash)
 }
 
-// defaultHandler returns the main json data which will be used to render the main page
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := session.Store.Get(r, "user")
+func comparePasswords(hashedPwd string, plainPwd []byte) bool {
+	// Since we'll be getting the hashed password from the DB it
+	// will be a string so we'll need to convert it to a byte slice
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if session.Values["UserID"] != 1234 {
-		http.Redirect(w, r, "http://localhost:8080/login", http.StatusTemporaryRedirect)
-		return
+		log.Println(err)
+		return false
 	}
 
-	w.Write([]byte("home handler"))
+	return true
 }
